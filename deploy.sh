737e2690ff0953262ada959d55c1012cae43c58e
duckulaj <jhawkins63@hotmail.com>
2026-01-27 12:58:23 +0000

    feat: implement client-side date range picker and stay management UI with new script and style fragments.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mvn clean package
 cd target
-# echo "Stopping M3UToolsJPA"
-# sudo systemctl stop M3UToolsJPA
-# rm /home/jonathan/M3UToolsJPA/M3UToolsJPA.log.*.gz
-# cp M3uJpa-0.0.1-SNAPSHOT.jar /home/jonathan/M3UToolsJPA/M3UToolsJPA.jar
-# echo "Starting M3UToolsJPA"
-# sudo systemctl start M3UToolsJPA
+echo "Stopping SchengenDays service"
+sudo systemctl stop schengendays
+rm /home/jonathan/SchengenDays/*.log.*.gz
+cp schengen-days-0.0.1-SNAPSHOT.jar /home/jonathan/SchengenDays/SchengenDays.jar
+echo "Starting SchengenDays service"
+sudo systemctl start schengendays
 
 
 # cd /home/jonathan/videoDownloader
